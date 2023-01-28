c218a1b4e90192dc1b3e1eef97b805cc8d5c256f
elnurdev <elnuranvarbek@mail.ru>
2023-01-28 15:11:05 +0300

    addded errors

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let span = document.querySelectorAll('.arrow')
+let div = document.querySelectorAll('.blog')
+let spanArrow = document.querySelectorAll('.stock-arrow')
+let divBlog = document.querySelectorAll('.stock-blog')
+let l = 0
+
+span [1].onclick = () => {
+  l++
+  for(let i of div) {
+    if (l==0) {i.style.left = '0px'}
+    if (l==1) {i.style.left = '-549px'}
+    if (l==2) {i.style.left = '-1098px'}
+    if (l==3) {i.style.left = '-1647px'}
+    if (l>4) {l=4}
+  }
+}
+
+span [0].onclick = () => {
+  l--
+  for(let i of div) {
+    if (l==0) {i.style.left = '0px'}
+    if (l==1) {i.style.left = '-549px'}
+    if (l==2) {i.style.left = '-1098px'}
+    if (l==3) {i.style.left = '-1647px'}
+    if (l<0) {l=0}
+  }
+}
+
+spanArrow [1].onclick = () => {
+  l++
+  for(let i of divBlog) {
+    if (l==0) {i.style.left = '0px'}
+    if (l==1) {i.style.left = '-1200px'}
+    if (l>4) {l=4}
+  }
+}
+
+spanArrow [0].onclick = () => {
+  l--
+  for(let i of divBlog) {
+    if (l==0) {i.style.left = '0px'}
+    if (l==1) {i.style.left = '-1200px'}
+    if (l<0) {l=0}
+  }
+}
+
