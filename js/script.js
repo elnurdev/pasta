24a826480c76a5315dd7a4c8f844823d62e3ce16
elnurdev <elnuranvarbek@mail.ru>
2023-01-29 08:56:14 +0300

    added btn

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let span = document.querySelectorAll('.arrow')
 let div = document.querySelectorAll('.blog')
 let spanArrow = document.querySelectorAll('.stock-arrow')
 let divBlog = document.querySelectorAll('.stock-blog')
+const active = document.querySelector('.stock-active')
 let l = 0
 
 span [1].onclick = () => {
@@ -30,7 +31,7 @@ spanArrow [1].onclick = () => {
   l++
   for(let i of divBlog) {
     if (l==0) {i.style.left = '0px'}
-    if (l==1) {i.style.left = '-1200px'}
+    if (l==1) {i.style.left = '-120vh'}
     if (l>4) {l=4}
   }
 }
@@ -39,8 +40,23 @@ spanArrow [0].onclick = () => {
   l--
   for(let i of divBlog) {
     if (l==0) {i.style.left = '0px'}
-    if (l==1) {i.style.left = '-1200px'}
+    if (l==1) {i.style.left = '-120vh'}
     if (l<0) {l=0}
   }
+  spanArrow.addEventListener('click', () => {
+    spanArrow.classList.remove('stock-active')
+  })
 }
 
+for (const active of spanArrow) {
+  active.addEventListener('click', () => {
+    clearActiveClasses()
+    active.classList.add('stock-active')
+  })
+}
+
+function clearActiveClasses() {
+  spanArrow.forEach((active) => {
+    active.classList.remove('stock-active')
+  })
+}
